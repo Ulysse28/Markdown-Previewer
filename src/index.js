1421abc942bc9abec06d335c39d0c68118fb2ef2
ulysse28 <uvaldenaire@gmail.com>
2021-09-20 20:16:33 +0200

    project's end

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import marked from "marked";
 
+import "./index.css";
+
 class App extends React.Component {
   state = {
-    output: marked("# e"),
-    newText: "",
+    output: marked(mardown),
+    newText: mardown,
   };
 
   createMarkup(text) {
@@ -22,14 +24,18 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Markdown</h1>
+      <div class="bloc">
+        <h1 id="title">Markdown Previewer</h1>
+        <h2>Mardown Editor</h2>
         <textarea
+          id="editor"
           onChange={this.handleChange.bind(this)}
           placeholder="ee"
           value={this.state.newText}
         ></textarea>
+        <h2>Markdown Previewer</h2>
         <div
+          id="preview"
           dangerouslySetInnerHTML={this.createMarkup(this.state.output)}
         ></div>
       </div>
@@ -37,5 +43,22 @@ class App extends React.Component {
   }
 }
 
+const mardown = `# This is a h1 title 
+## This is a h2 title
+This is my  [portofolio](https://ulysse28.github.io/Portofolio-Ulysse-Valdenaire/),
+inline code, \`<div></div>\`
+\`\`\`
+// this is multi-line code:
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+> This is a block quote
+- This is a list
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+And this is **bold text** !
+`;
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
